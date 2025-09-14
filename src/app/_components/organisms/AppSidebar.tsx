dbad29974114ffff3c0905f922282372650f79b5
Matheus Almeida <dev.matheus.almeida@gmail.com>
2025-09-14 00:17:57 -0300

    feat: add sidebar

---
+++ src/app/_components/organisms/AppSidebar.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import NextImage from "next/image";
+
+import { LucideIcon, Plus, Workflow } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+} from "../ui/sidebar";
+
+interface Item {
+  title: string;
+  onClick: () => void;
+  icon: LucideIcon;
+  buttonIcon: LucideIcon;
+}
+
+const itemsMenu: Item[] = [
+  {
+    title: "Process",
+    onClick: () => {
+      console.log("Should Open a modal to create a process");
+    },
+    icon: Workflow,
+    buttonIcon: Plus,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader className="flex items-center justify-center my-4">
+        <NextImage
+          src="/vercel.svg"
+          alt="Logo"
+          width={32}
+          height={32}
+          className="rounded"
+        />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Menu</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {itemsMenu.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <div className="flex items-center gap-2">
+                      <item.icon className="size-5" />
+                      <span>{item.title}</span>
+                    </div>
+                  </SidebarMenuButton>
+
+                  <SidebarMenuAction>
+                    <item.buttonIcon
+                      className="cursor-pointer size-4"
+                      onClick={item.onClick}
+                    />
+                  </SidebarMenuAction>
+
+                  <SidebarMenuSub>
+                    <SidebarMenuItem>
+                      <SidebarMenuSubButton>1</SidebarMenuSubButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuSubButton>1</SidebarMenuSubButton>
+                    </SidebarMenuItem>
+                  </SidebarMenuSub>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
--- src/app/template/HomeTemplate.tsx
+++ src/app/template/HomeTemplate.tsx
@@ -1,7 +1,15 @@
+import { SidebarProvider, SidebarTrigger } from "../_components/ui/sidebar";
+import { AppSidebar } from "@/app/_components/organisms/AppSidebar";
+
 export default function HomeTemplate() {
   return (
     <>
-      <h1>Welcome to the Home Page</h1>
+      <SidebarProvider>
+        <AppSidebar />
+
+        <SidebarTrigger className="m-4" />
+        <h1>Welcome to the Home Page aaa</h1>
+      </SidebarProvider>
     </>
   );
 }
