0edc22b0b3aa90d74067bd484bca786f321b6906
Matheus Almeida <dev.matheus.almeida@gmail.com>
2025-09-14 13:27:07 -0300

    feat: create loading component

---
+++ src/app/_components/atoms/Loading.tsx
@@ -0,0 +1,9 @@
+import { Loader } from "lucide-react";
+
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center">
+      <Loader className="animate-spin" />
+    </div>
+  );
+}
