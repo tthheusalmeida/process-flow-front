707b37fadccd5efc6b860618453aef1a3439e275
Matheus Almeida <dev.matheus.almeida@gmail.com>
2025-09-13 22:51:30 -0300

    feat: setup shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} antialiased`}>{children}</body>
+      <body className={`${roboto.variable} antialiased dark`}>{children}</body>
     </html>
   );
 }
